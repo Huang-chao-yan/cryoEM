ce8c1a47c76605138b3b8e617d48a936e0fd3a72
Huang-Chao-yan <64688189+Huang-chao-yan@users.noreply.github.com>
2023-06-12 18:19:27 +0800

    Update readme.m

--- readme.m
+++ readme.m
@@ -3,3 +3,8 @@ Please add all the files into your path
 Please run cryoEM_my_final.m for reference. 
 By chaoyan
 Feb 1, 2023
+
+
+If there are some codes missing, please refer https://drive.google.com/drive/folders/1-dSDD4T89dV20YpJjx1D8xwOyUyBVJG3?usp=sharing
+By chaoyan
+Jun 12, 2023
